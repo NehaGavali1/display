cc6722e8bb74ea7264f7e61f426045bebef44606
NehaGavali1 <nehagavali1@gmail.com>
2023-07-12 13:23:13 +0530

    done

---
+++ script.js
@@ -0,0 +1,109 @@
+
+         function run(){
+        let tap1=document.getElementById("show").innerHTML="A";
+        document.getElementById("show").style="font-size:200px;"
+    }
+    
+    function run1(){
+        let tap1=document.getElementById("show").innerHTML="B";
+        document.getElementById("show").style="font-size:200px;"
+    }
+    function run2(){
+        let tap1=document.getElementById("show").innerHTML="C";
+        document.getElementById("show").style="font-size:200px;"
+    }
+    function run3(){
+        let tap1=document.getElementById("show").innerHTML="D";
+        document.getElementById("show").style="font-size:200px;"
+    }
+    function run4(){
+        let tap1=document.getElementById("show").innerHTML="E";
+        document.getElementById("show").style="font-size:200px;"
+    }
+    function run5(){
+        let tap1=document.getElementById("show").innerHTML="F";
+        document.getElementById("show").style="font-size:200px;"
+    }
+
+    function run6(){
+        let tap1=document.getElementById("show").innerHTML="G";
+        document.getElementById("show").style="font-size:200px;"
+    }
+    function run7(){
+        let tap1=document.getElementById("show").innerHTML="H";
+        document.getElementById("show").style="font-size:200px;"
+    }
+    function run8(){
+        let tap1=document.getElementById("show").innerHTML="I";
+        document.getElementById("show").style="font-size:200px;"
+    }
+    function run9(){
+        let tap1=document.getElementById("show").innerHTML="J";
+        document.getElementById("show").style="font-size:200px;"
+    }
+    function run10(){
+        let tap1=document.getElementById("show").innerHTML="K";
+        document.getElementById("show").style="font-size:200px;"
+    }
+    function run11(){
+        let tap1=document.getElementById("show").innerHTML="L";
+        document.getElementById("show").style="font-size:200px;"
+    }
+    function run12(){
+        let tap1=document.getElementById("show").innerHTML="M";
+        document.getElementById("show").style="font-size:200px;"
+    }
+    function run13(){
+        let tap1=document.getElementById("show").innerHTML="N";
+        document.getElementById("show").style="font-size:200px;"
+    }
+    function run14(){
+        let tap1=document.getElementById("show").innerHTML="O";
+        document.getElementById("show").style="font-size:200px;"
+    }
+    function run15(){
+        let tap1=document.getElementById("show").innerHTML="P";
+        document.getElementById("show").style="font-size:200px;"
+    }
+    function run16(){
+        let tap1=document.getElementById("show").innerHTML="Q";
+        document.getElementById("show").style="font-size:200px;"
+    }
+    function run17(){
+        let tap1=document.getElementById("show").innerHTML="R";
+        document.getElementById("show").style="font-size:200px;"
+    }
+    function run18(){
+        let tap1=document.getElementById("show").innerHTML="S";
+        document.getElementById("show").style="font-size:200px;"
+    }
+    function run19(){
+        let tap1=document.getElementById("show").innerHTML="T";
+        document.getElementById("show").style="font-size:200px;"
+    }
+    function run20(){
+        let tap1=document.getElementById("show").innerHTML="U";
+        document.getElementById("show").style="font-size:200px;"
+    }
+    function run21(){
+        let tap1=document.getElementById("show").innerHTML="V";
+        document.getElementById("show").style="font-size:200px;"
+    }
+    function run22(){
+        let tap1=document.getElementById("show").innerHTML="W";
+        document.getElementById("show").style="font-size:200px;"
+    }
+    function run23(){
+        let tap1=document.getElementById("show").innerHTML="X";
+        document.getElementById("show").style="font-size:200px;"
+       
+    }
+    function run24(){
+        let tap1=document.getElementById("show").innerHTML="Y";
+        document.getElementById("show").style="font-size:200px;"
+    }
+    function run25(){
+        let tap1=document.getElementById("show").innerHTML="Z";
+        document.getElementById("show").style="font-size:200px;"
+    }
+
